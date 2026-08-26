fa9ac0ca4b8b2409b069a356321f96e1fc1f550b
pedronovaes <pedronovaesmelo@gmail.com>
2026-08-26 20:14:00 -0300

    feat: add base transformations and a base model factory function

--- src/models/model.py
+++ src/models/model.py
@@ -1,1 +1,35 @@
-# Deep-learning architetures.
+"""
+3D MRI classification model factory using MONAI backbones.
+"""
+
+from torch import nn
+from monai.networks.nets import resnet18, DenseNet121, EfficientNetBN
+
+
+def get_model(model_name: str, num_classes: int = 2) -> nn.Module:
+    """Returns a MONAI 3D classification model by name."""
+
+    models = {
+        'densenet121': lambda: DenseNet121(
+            spatial_dims=3,
+            in_channels=1,
+            out_channels=num_classes
+        ),
+        'resnet18': lambda: resnet18(
+            spatial_dims=3,
+            n_input_channels=1,
+            num_classes=num_classes
+        ),
+        'efficientnet-b0': lambda: EfficientNetBN(
+            model_name='efficientnet-b0',
+            spatial_dims=3,
+            in_channels=1,
+            num_classes=num_classes,
+            pretrained=False
+        )
+    }
+
+    if model_name not in models:
+        raise ValueError(f"Invalid '{model_name}' model.")
+
+    return models[model_name]()
--- src/preprocessing/transforms.py
+++ src/preprocessing/transforms.py
@@ -1,1 +1,26 @@
-# Pipelines MONAI.
+# type: ignore
+import monai.transforms as mt
+
+
+def get_transforms(preset: str = 'baseline') -> mt.Compose:
+    """Returnsthe transforms pipeline."""
+
+    base_transforms = [
+        mt.LoadImaged(keys=['image']),
+        mt.EnsureChannelFirstd(keys=['image']),  # Adjust the channel dimension of input data.
+        mt.Orientationd(keys=['image'], axcodes='RAS'),
+        mt.Spacingd(
+            keys=['image'],
+            pixdim=(1.0, 1.0, 1.0),  # Output voxel spacing.
+            mode=['bilinear']
+        ),
+        mt.NormalizeIntensityd(keys=['image'], nonzero=True, channel_wise=True)
+    ]
+
+    # New transformations, such as augmentation, will come here.
+
+    base_transforms.append(
+        mt.EnsureTyped(keys=['image'])  # Ensure the input data to be a PyTorch tensor.
+    )
+
+    return mt.Compose(transforms=base_transforms)
